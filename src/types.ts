f619cb52691433e7c4590427200729eb5b14ab16
Melwyn Saldanha <melwyn95@gmail.com>
2023-05-04 12:39:53 +0530

    feat: define data classes for Types

---
+++ src/types.ts
@@ -0,0 +1,74 @@
+interface Type {
+    equals(other: Type): boolean;
+    toString(): string;
+}
+
+class BooleanType implements Type {
+    equals(other: Type): boolean {
+        return other instanceof BooleanType
+    }
+
+    toString(): string {
+        return "boolean"
+    }
+}
+
+class NumberType implements Type {
+    equals(other: Type): boolean {
+        return other instanceof NumberType
+    }
+
+    toString(): string {
+        return "number"
+    }
+}
+
+class VoidType implements Type {
+    equals(other: Type): boolean {
+        return other instanceof VoidType
+    }
+
+    toString(): string {
+        return "void"
+    }
+}
+
+class ArrayType implements Type {
+    constructor(public element: Type) { }
+
+    equals(other: Type): boolean {
+        return other instanceof ArrayType &&
+            this.element.equals(other.element)
+    }
+
+    toString(): string {
+        return `Array<${this.element}>`
+    }
+}
+
+class FunctionType implements Type {
+    constructor(
+        public parameters: Map<string, Type>,
+        public returnType: Type
+    ) { }
+
+    equals(other: Type): boolean {
+        if (other instanceof FunctionType) {
+            let otherParameterTypes = Array.from(other.parameters.values())
+            let thisParameterTypes = Array.from(this.parameters.values())
+            return this.parameters.size === other.parameters.size
+                && this.returnType.equals(other.returnType)
+                && thisParameterTypes.every(
+                    (p, i) => p.equals(otherParameterTypes[i]))
+        } else {
+            return false
+        }
+    }
+
+    toString(): string {
+        let parameters = Array.from(this.parameters).map(
+            ([name, type]) => `${name}: ${type}`
+        )
+        return `(${parameters.join(", ")}) => ${this.returnType}`
+    }
+}
